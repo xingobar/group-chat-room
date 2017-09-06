558860380fd6f5b6c29849909636b71719334f2b
xingobar <gary840227@gmail.com>
2017-09-07 00:21:52 +0800

    GroupCreated implement ShouldBroadcast

--- GroupChatRoom/app/Events/GroupCreated.php
+++ GroupChatRoom/app/Events/GroupCreated.php
@@ -11,7 +11,7 @@ use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use App\Group;
 
-class GroupCreated
+class GroupCreated implements  ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
